6d4324ca7299c719d9dae6108db0dcd70a6a1554
littmus <sdw2648@gmail.com>
2015-05-27 21:01:57 +0900

    Remove py3 requirements dependency

--- setup.py
+++ setup.py
@@ -1,23 +1,9 @@
 #! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-import os
 import sys
 from setuptools import find_packages, setup
 from konlpy import __version__
-
-def requirements():
-    def _openreq(reqfile):
-        with open(os.path.join(os.path.dirname(__file__), reqfile)) as f:
-            return f.read().splitlines()
-
-    def _genver(major, minorlist):
-        return ':%s' % ' or '.join('python_version=="%s.%s"' % (major, i) for i in minorlist)
-
-    return {
-        _genver(2, [6,7]): _openreq('requirements.txt'),
-        _genver(3, range(5)): _openreq('requirements-py3.txt')
-    }
 
 setup(name='konlpy',
     version=__version__,
@@ -63,5 +49,4 @@ KoNLPy is not just to create another, but to unify and build upon their shoulder
         'java/*.jar',
         'java/bin/kr/lucypark/*/*.class',
         'java/bin/kr/lucypark/*/*/*.class',
-        ]},
-    extras_require=requirements())
+        ]})
